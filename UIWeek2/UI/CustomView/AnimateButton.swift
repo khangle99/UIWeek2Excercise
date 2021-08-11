290929a1ac6e8751603bdb9f6c4c38c519f61d02
khangle99 <dev.khangle@gmail.com>
2021-08-11 14:15:02 +0700

    fix(core): stop user interact when animate button click

--- UIWeek2/UI/CustomView/AnimateButton.swift
+++ UIWeek2/UI/CustomView/AnimateButton.swift
@@ -2,17 +2,22 @@ import Foundation
 import UIKit
 
 class AnimateButton: UIButton {
+    let scaleFactor: CGFloat = 0.9
+    let animDuration = 0.1
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         addTarget(self, action: #selector(animateButton(_:)), for: .touchUpInside)
     }
     
     @objc func animateButton(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.2) {
-            self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-        } completion: { (completed) in
-            UIView.animate(withDuration: 0.2) {
+        isUserInteractionEnabled = false
+        UIView.animate(withDuration: animDuration) { [unowned self] in
+            self.transform = CGAffineTransform(scaleX: self.scaleFactor, y: self.scaleFactor)
+        } completion: { [unowned self] (completed) in
+            UIView.animate(withDuration: self.animDuration) {
                 self.transform = .identity
+            } completion: { (completed) in
+                self.isUserInteractionEnabled = true
             }
         }
     }
